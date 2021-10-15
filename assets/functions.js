6595272cbd05569722ca6b41371584f8edf6fbad
chiwaki hajime <erehajime@gmail.com>
2021-10-15 15:17:26 +0900

    add css/js
    additional styles

---
+++ assets/functions.js
@@ -0,0 +1,33 @@
+//! ======================================================
+//!
+//! function.js
+//!
+//! ======================================================
+
+
+
+
+$(function(){
+
+  objectFitImages();
+
+  var disableBodyScroll = bodyScrollLock.disableBodyScroll;
+  var enableBodyScroll = bodyScrollLock.enableBodyScroll;
+
+  var urlHash = location.hash;
+
+  var body = $('body');
+  var menu = $('#menu');
+  var nav = $('#nav');
+  var header = $('#header');
+
+  $('.product_header_imgs').slick({
+    fade: true,
+    arrows: false
+  })
+
+
+
+});
+
+
