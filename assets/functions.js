394046f09d5c3c7dbb6a89a1ab7707eba2924f1b
chiwaki hajime <erehajime@gmail.com>
2022-02-26 00:09:24 +0900

    masonry loaded items

--- assets/functions.js
+++ assets/functions.js
@@ -48,9 +48,11 @@ $(function(){
     checkbp();
   })
 
-
   $('[data-tile]').each(function(){
+    var media_num = 0;
     $(this).imagesLoaded().done( function( instance ) {
+      media_num = nedia_num + $(this).find('img').length;
+      alert(media_num);
       $(instance.elements[0]).masonry({
         itemSelector: '.tile_item'
       })
